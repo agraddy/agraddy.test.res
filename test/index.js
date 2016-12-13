fc6f3abc2d94a25da3ce2fc6dab754c9c6a2eebc
Anthony Graddy <anthony@locationq.com>
2016-12-13 21:05:19 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,10 +1,42 @@
 var test = require('tape');
 
-var mod = require('../');
+var mod;
 
 test('overall', function(t) {
-	t.equal(mod.main('result'), 'result');
+	mod = require('../')();
+	t.equal(typeof mod.end, 'function');
+	t.equal(typeof mod.setHeader, 'function');
+	t.equal(typeof mod.writeHead, 'function');
+	t.equal(typeof mod.write, 'function');
+	t.equal(typeof mod._body, 'string');
+	t.equal(typeof mod._headers, 'object');
+	t.equal(typeof mod._statusCode, 'number');
 	t.end();
 });
 
+test('setHeader', function(t) {
+	mod = require('../')();
+	mod.setHeader('Content-Type', 'application/javascript');
+
+	t.deepEqual(mod._headers, [{'Content-Type': 'application/javascript'}]);
+	t.end();
+});
+
+test('write', function(t) {
+	mod = require('../')();
+	mod.write('test');
+
+	t.equal(mod._body, 'test');
+	t.end();
+});
+
+test('writeHead', function(t) {
+	mod = require('../')();
+	mod.writeHead(200, {'Content-Type': 'application/javascript'});
+
+	t.equal(mod._body, '');
+	t.deepEqual(mod._headers, [{'Content-Type': 'application/javascript'}]);
+	t.equal(mod._statusCode, 200);
+	t.end();
+});
 
